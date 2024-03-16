d0fede6e89b614033a550334b2443637a17114e1
Jamie Thomas Grantham-Burton <jgranthamburton@Jamies-MacBook-Air>
2024-03-16 13:11:16 +1100

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import ListingCard from './components/listings/ListingCard';
+
+function App() {
+  return (
+    <>
+      <ListingCard name="test 1"/>
+    </>
+  );
+}
+
+export default App;

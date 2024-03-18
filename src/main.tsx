be57c38d0b94e13049207f4f8e8d00f3490ba0dd
Jamie Thomas Grantham-Burton <jgranthamburton@Jamies-MacBook-Air>
2024-03-18 14:19:04 +1100

    added dev tools for react query

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx';
 import './index.css';
 import { QueryClient } from '@tanstack/query-core';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 );
